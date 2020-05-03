5012bbbd7e537a83a32311cc68e9459a01a17434
viei0628 <47645893+viei0628@users.noreply.github.com>
2020-05-03 16:15:39 -0400

    Update fibonacci.rb

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,7 +2,8 @@
 #func fibs sums each number with number before it.
 
 def fibs(n)
-  return 1 ? n == 0 or n == 1 : fibs(n-2) + fibs(n-1)
+  return 1 if n == 0 or n == 1 
+  fibs(n-2) + fibs(n-1)
 end
 
 p fibs(5)
